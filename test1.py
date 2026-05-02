8faa28732cf5002793c9282cc70e18999a2e98dd
byteUUM <xyk3387235752@qq.com>
2026-05-02 22:43:11 +0800

    开发环境搭建与浏览器驱动

---
+++ test1.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# 驱动管理
+ChromeIns = ChromeDriverManager().install()
+
+driver = webdriver.Chrome(service=Service(ChromeIns))
+driver.get("https://www.baidu.com")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "#chat-textarea").send_keys("周星驰")
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR,"#chat-submit-button").click()
+driver.quit()
