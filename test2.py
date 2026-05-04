0adbb003f132c538fcfc751480f1196788cf4535
byteUUM <xyk3387235752@qq.com>
2026-05-04 09:24:15 +0800

    update

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from selenium import webdriver
@@ -11,7 +12,9 @@ from selenium.webdriver.common.keys import Keys
 # 获取浏览器驱动
 ChromeIns = ChromeDriverManager().install()
 # 创建驱动对象
-driver = webdriver.Chrome(service=Service(ChromeIns))
+options = webdriver.ChromeOptions()
+# options.add_argument("-headless") #无头模式
+driver = webdriver.Chrome(service=Service(ChromeIns),options=options)
 wait = WebDriverWait(driver, 10)
 # driver.get("https://www.baidu.com")
 # wu = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#chat-textarea")))
@@ -31,18 +34,30 @@ wait = WebDriverWait(driver, 10)
 # text = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#hotsearch-content-wrapper > li:nth-child(1) > a > span.title-content-title"))).text
 # print(text)
 # driver.quit()
-driver.get("https://www.douyin.com/jingxuan")
-time.sleep(60)
-tj = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#douyin-navigation > div > div.MKOzvYDg.Yr4fQlKQ > div > div:nth-child(1) > div.DlKicC22 > div > div:nth-child(2) > div > div > a")))
-tj.click()
+# driver.get("https://www.douyin.com/jingxuan")
+# time.sleep(60)
+# tj = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#douyin-navigation > div > div.MKOzvYDg.Yr4fQlKQ > div > div:nth-child(1) > div.DlKicC22 > div > div:nth-child(2) > div > div > a")))
+# tj.click()
 # dz = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"#sliderVideo > div.E7R0E__S.playerContainer.hide-animation-if-not-suport-gpu.TkocvtkE > div > div.vqN35AZ4.basePlayerContainer.xg5nzy2Q.chapterPlayerStyle.MediaNotSupportStyle.lowPopup > div.i2VIB6P0 > div > div > div.zqe4B9aR.WU6dkKao > div:nth-child(2) > div > div.UIQajZAR > div > svg")))
 # dz.click()
-time.sleep(10)
-body = driver.find_element(By.TAG_NAME, 'body')
-for i in range(0,10):
-    ActionChains(driver).send_keys("z").perform()
-    body.send_keys(Keys.ARROW_DOWN)
-    # ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
-    time.sleep(1)
-time.sleep(15)
+# time.sleep(10)
+# body = driver.find_element(By.TAG_NAME, 'body')
+# for i in range(0,10):
+#     ActionChains(driver).send_keys("z").perform()
+#     body.send_keys(Keys.ARROW_DOWN)
+#     # ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+#     time.sleep(1)
+# time.sleep(15)
+# driver.quit()
+
+driver.get("https://www.baidu.com")
+# driver.maximize_window()
+wu = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#chat-textarea")))
+wu.send_keys("罗伯托·贝尼尼")
+wu2 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#chat-submit-button")))
+wu2.click()
+# time.sleep(3)
+image_name = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S") + ".png"
+# driver.save_screenshot("./images/"+image_name)
 driver.quit()
+
